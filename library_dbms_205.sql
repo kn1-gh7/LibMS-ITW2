85827d6c7e3a0c8b2a14fd0dffceb866fc7b7175
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-11-05 18:59:20 +0530

    Update DB

--- library_dbms_205.sql
+++ library_dbms_205.sql
@@ -1,11 +1,12 @@
 USE library_205;
-DROP TABLE IF EXISTS genres, books, users,library_db, transaction_types, lib_transactions;
+DROP TABLE IF EXISTS genres, books, users,library_db, transaction_types, lib_transactions, book_genres;
 
 CREATE TABLE genres (
     label INT UNSIGNED,
     categories VARCHAR(40) NOT NULL,
     PRIMARY KEY (label)
 );
+
 /*  
 	The genre table is more like flag enum for the categories in the books table.
     For example a category = 37 = 0b100101 will mean it belongs to genre 0, 2, and 5    
@@ -37,16 +38,26 @@ CREATE TABLE genres (
 (25, "Academic literature")
 */
 
+
+
 CREATE TABLE books (
     ISBN VARCHAR(13),
     title VARCHAR(100) NOT NULL,
     author VARCHAR(50),
-    genre INT UNSIGNED,
+    -- genre INT UNSIGNED,
     year_published MEDIUMINT UNSIGNED, -- NOTE: THE YEAR DATATYPE IS NOT USED SINCE IT CAN'T STORE YEARS BEFORE 1900s
     description_text VARCHAR(200),
     PRIMARY KEY (ISBN)
 );
-
+CREATE TABLE book_genres (
+	book_ISBN VARCHAR(13),
+    genre INT UNSIGNED,
+    foreign key (genre) 
+    references genres (label),
+    
+    foreign key (book_ISBN)
+    references books (ISBN)
+);
 CREATE TABLE users (
     user_id INT UNSIGNED,
     name VARCHAR(50) NOT NULL,
@@ -88,16 +99,16 @@ CREATE TABLE lib_transactions (
 INSERT INTO genres VALUES (0, "Others"), (1, "Fiction"), (2, "Mystery"), (3, "Fantasy"), (4, "Horror"), (5, "Nonfiction"), (6, "Romance"), (7, "Thriller"), (8, "Science fiction"), (9, "Historical fiction"), (10, "Short story"), (11, "Literary fiction"), (12, "Poetry"), (13, "Graphic novel"), (14, "Biography"), (15, "Young adult"), (16, "Memoir"), (17, "Dystopia"), (18, "Magical realism"), (19, "Children literature"), (20, "Adventure fiction"), (21, "Action fiction"), (22, "Contemporary literature"), (23, "Paranormal romance"), (24, "Women fiction"), (25, "Academic literature");
 INSERT INTO books
 VALUES
-    ('9780451524935', 'To Kill a Mockingbird', 'Harper Lee'    , 1, 1960, 'A classic novel about racial injustice in the American South.'),
-    ('9780141983769', '1984', 'George Orwell'                  , 2, 1949, 'A dystopian novel depicting a totalitarian society.'),
-    ('9780061120084', 'The Catcher in the Rye', 'J.D. Salinger', 1, 1951, 'A coming-of-age novel about a disenchanted teenager.'),
-    ('9780062315007', 'The Great Gatsby', 'F. Scott Fitzgerald', 2, 1925, 'A story of wealth, excess, and the American Dream.'),
-    ('9780141439563', 'Pride and Prejudice', 'Jane Austen'     , 3, 1813, 'A romantic novel revolving around the Bennet family.'),
-    ('9780451527745', 'Moby-Dick', 'Herman Melville'           , 4, 1851, 'An epic tale of a man\'s obsession with a white whale.'),
-    ('9780060256654', 'Alice\'s Adventures in Wonderland', 'Lewis Carroll', 5, 1865, 'A whimsical fantasy story about a girl named Alice.'),
-    ('9780141439594', 'Jane Eyre', 'Charlotte Brontë'          , 3, 1847, 'A Gothic romance novel featuring the character Jane Eyre.'),
-    ('9780451524936', 'The Hobbit', 'J.R.R. Tolkien'           , 5, 1937, 'A fantasy novel about the adventures of Bilbo Baggins.'),
-    ('9780061120060', 'The Lord of the Rings', 'J.R.R. Tolkien', 5, 1954, 'A high-fantasy epic set in the fictional world of Middle-earth.');
+    ('9780451524935', 'To Kill a Mockingbird', 'Harper Lee'    ,  1960, 'A classic novel about racial injustice in the American South.'),
+    ('9780141983769', '1984', 'George Orwell'                  ,  1949, 'A dystopian novel depicting a totalitarian society.'),
+    ('9780061120084', 'The Catcher in the Rye', 'J.D. Salinger',  1951, 'A coming-of-age novel about a disenchanted teenager.'),
+    ('9780062315007', 'The Great Gatsby', 'F. Scott Fitzgerald',  1925, 'A story of wealth, excess, and the American Dream.'),
+    ('9780141439563', 'Pride and Prejudice', 'Jane Austen'     ,  1813, 'A romantic novel revolving around the Bennet family.'),
+    ('9780451527745', 'Moby-Dick', 'Herman Melville'           ,  1851, 'An epic tale of a man\'s obsession with a white whale.'),
+    ('9780060256654', 'Alice\'s Adventures in Wonderland', 'Lewis Carroll',  1865, 'A whimsical fantasy story about a girl named Alice.'),
+    ('9780141439594', 'Jane Eyre', 'Charlotte Brontë'          ,  1847, 'A Gothic romance novel featuring the character Jane Eyre.'),
+    ('9780451524936', 'The Hobbit', 'J.R.R. Tolkien'           ,  1937, 'A fantasy novel about the adventures of Bilbo Baggins.'),
+    ('9780061120060', 'The Lord of the Rings', 'J.R.R. Tolkien',  1954, 'A high-fantasy epic set in the fictional world of Middle-earth.');
 
 INSERT INTO users
 VALUES
@@ -156,4 +167,3 @@ CREATE TRIGGER count_check BEFORE INSERT ON lib_transactions
 		END;
 |
 delimiter ;
-
